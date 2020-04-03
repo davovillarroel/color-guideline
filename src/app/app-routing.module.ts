88f49cd14caab5f1d0341adbe2ede8fd8f5d580d
David Alfredo Villarroel Contreras <dav.villarroel@gmai.com>
2020-04-03 09:06:52 -0300

    corrección home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './views/Home/home.component';
+import { HomeComponent } from './views/home/home.component';
 import { ColorsComponent } from './views/colors/colors.component';
 
 
